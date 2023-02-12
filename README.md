feee4501c1b2c3a9c7af907067d7f414771347cc
Jitsem <jitse.meulenijzer@gmail.com>
2023-02-12 14:06:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# portfolio-web
+[![Netlify Status](https://api.netlify.com/api/v1/badges/cb2ad285-c38e-4e89-be73-210cdecb2e73/deploy-status)](https://app.netlify.com/sites/vigorous-engelbart-1c030c/deploys)
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+
+```
+To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	import '../app.css';
+	import Logo from '$lib/assets/logo.svg';
+</script>
+
+<div class="flex justify-between flex-wrap mx-16 md:px-10">
+	<img class="jm-logo" src={Logo} alt="jitsem logo" />
+	<div>
+		<ul class="flex">
+			<li
+				class="mx-3 text-xl md:text-3xl text-gray-700 no-underline lowercase hover:text-blue-700 transition duration-500"
+			>
+				<a href="/">Home</a>
+			</li>
+			<li
+				class="mx-3 text-xl md:text-3xl text-gray-700 no-underline lowercase hover:text-blue-700 transition duration-500"
+			>
+				<a href="/blog">Blog</a>
+			</li>
+			<li
+				class="mx-3 text-xl md:text-3xl text-gray-700 no-underline lowercase hover:text-blue-700 transition duration-500"
+			>
+				<a href="/portfolio">Portfolio</a>
+			</li>
+			<li
+				class="mx-3 text-xl md:text-3xl text-gray-700 no-underline lowercase hover:text-blue-700 transition duration-500"
+			>
+				<a href="/projects">Projects</a>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<slot />
